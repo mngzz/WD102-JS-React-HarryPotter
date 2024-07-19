1cb92ba0e5831cf10d5d85c7d8a6b9f3f54b32d7
mngzz <mngudez@gmail.com>
2024-07-19 12:00:10 +0800

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-// App.jsx
 import React from "react";
 import "./App.css";
-import Header from "./components/header/Header"; // Adjusted path to Header component
+import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Staff from "./components/staff/Staff";
